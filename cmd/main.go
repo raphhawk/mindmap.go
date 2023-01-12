6d69476d910062995f9e6302cb3ba2eba1bf2aa8
k <bing619.at@gmail.com>
2023-01-12 11:34:22 +0530

    init3

--- cmd/main.go
+++ cmd/main.go
@@ -10,48 +10,34 @@ import (
 
 var loc = os.Args[1]
 
-type Node struct {
-	data     []fs.FileInfo
-	nxtNodes []Node
-}
-
-func (dirs *Node) stretch() {
-	var s = []fs.FileInfo{dirs.data[0]}
-	for len(s) > 0 {
-		cur := s[len(s)-1]
-		s = s[:len(s)-1]
-		if cur.IsDir() {
-			var n Node
-			var err error
-			n.data, err = ioutil.ReadDir(loc + "/" + cur.Name())
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			dirs.nxtNodes = append(dirs.nxtNodes, n)
-		}
+func rgrab(f fs.FileInfo, h map[fs.FileInfo]string, baseDir string) {
+	if !f.IsDir() {
+		h[f] = baseDir + "/" + f.Name()
+		return
 	}
-}
-
-func grab(loc string) (Node, error) {
-	files, err := ioutil.ReadDir(loc)
+	dirString, err := ioutil.ReadDir(baseDir + "/" + f.Name())
 	if err != nil {
-		return Node{}, err
+		fmt.Println(err)
+		return
 	}
-	dirs := Node{
-		files,
-		[]Node{},
+	for _, i := range dirString {
+		rgrab(i, h, baseDir+"/"+f.Name())
 	}
-	dirs.stretch()
-	return dirs, nil
 }
 
 func main() {
-	files, err := grab(loc)
+	if len(os.Args) > 2 {
+		os.Exit(1)
+	}
+	files, err := ioutil.ReadDir(loc)
 	if err != nil {
 		log.Panic(err)
 	}
-	for _, i := range files.data {
-		fmt.Println(i.Name(), i.IsDir())
+	files2 := map[fs.FileInfo]string{}
+	for _, i := range files {
+		rgrab(i, files2, loc)
+	}
+	for _, i := range files2 {
+		fmt.Println(i)
 	}
 }
